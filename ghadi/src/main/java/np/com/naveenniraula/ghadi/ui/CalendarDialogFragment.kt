e16f9e2933edb15749e37f1ba2c0145f1d762936
N. Niraula <subtlenv@gmail.com>
2020-09-16 11:02:15 +0545

    using data binding

--- ghadi/src/main/java/np/com/naveenniraula/ghadi/ui/CalendarDialogFragment.kt
+++ ghadi/src/main/java/np/com/naveenniraula/ghadi/ui/CalendarDialogFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -19,29 +20,30 @@ import androidx.recyclerview.widget.RecyclerView
 import np.com.naveenniraula.ghadi.R
 import np.com.naveenniraula.ghadi.data.DateItem
 import np.com.naveenniraula.ghadi.data.GhadiResult
+import np.com.naveenniraula.ghadi.databinding.CalendarDialogFragmentBinding
 import np.com.naveenniraula.ghadi.listeners.DatePickCompleteListener
 import np.com.naveenniraula.ghadi.miti.Date
 import np.com.naveenniraula.ghadi.miti.DateUtils
-import np.com.naveenniraula.ghadi.utils.ColorUtil
 import np.com.naveenniraula.ghadi.utils.ConversionUtil
 import np.com.naveenniraula.ghadi.utils.Ui
 import java.util.*
+import kotlin.math.min
 
-class GhadiPickerFragment : DialogFragment() {
+class CalendarDialogFragment : DialogFragment() {
 
     companion object {
 
-        fun newInstance() = GhadiPickerFragment()
+        fun newInstance() = CalendarDialogFragment()
 
-        fun newInstance(date: Date): GhadiPickerFragment {
-            val ghadiPickerFragment = GhadiPickerFragment()
+        fun newInstance(date: Date): CalendarDialogFragment {
+            val ghadiPickerFragment = CalendarDialogFragment()
             ghadiPickerFragment.requestedDate = date.convertToEnglish()
             return ghadiPickerFragment
         }
 
         @Deprecated("Don't use this method. Pass in the Date instance instead.")
-        fun newInstance(year: Int, month: Int, day: Int): GhadiPickerFragment {
-            val ghadiPickerFragment = GhadiPickerFragment()
+        fun newInstance(year: Int, month: Int, day: Int): CalendarDialogFragment {
+            val ghadiPickerFragment = CalendarDialogFragment()
             ghadiPickerFragment.requestedDate = Date(year, month, day).convertToEnglish()
             return ghadiPickerFragment
         }
@@ -50,6 +52,8 @@ class GhadiPickerFragment : DialogFragment() {
         const val DAYS_START_NUM = 1
     }
 
+    private lateinit var mBinding: CalendarDialogFragmentBinding
+
     var bgFgColor: Pair<Int, Int> = Pair(Color.BLACK, Color.WHITE)
 
     private val adapter = NepaliDateAdapter<DateItem>()
@@ -57,11 +61,18 @@ class GhadiPickerFragment : DialogFragment() {
     private lateinit var mFragmentManager: FragmentManager
 
     private lateinit var requestedDate: Date
-    private lateinit var viewModel: GhadiPickerViewModel
+    private lateinit var viewModel: CalendarDialogViewModel
     private lateinit var datePickCompleteListener: DatePickCompleteListener
 
+    /**
+     * This block of variable are for dynamically adjusting the recycler view's height each time new
+     * data is prepared.
+     */
+    private var minHeight = 0
+    private var lastHeight = 0
+    private var addedHeight = 0
+
     private lateinit var alertDialog: AlertDialog
-    private lateinit var rootView: View
 
     private val currentDateInNepali = Date(Calendar.getInstance()).convertToNepali()
 
@@ -77,14 +88,20 @@ class GhadiPickerFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         context?.let {
             val inflater = LayoutInflater.from(it)
-            rootView = inflater.inflate(R.layout.ghadi_picker_fragment, null, false)
+            mBinding = CalendarDialogFragmentBinding.inflate(inflater)
 
             setupNepaliDate()
 
             setupListeners()
 
+            mBinding.materialButtonToggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+                if (isChecked)
+                    Toast.makeText(requireContext(), "we have id : $checkedId", Toast.LENGTH_SHORT)
+                        .show()
+            }
+
             val builder = AlertDialog.Builder(it)
-            builder.setView(rootView)
+            builder.setView(mBinding.root)
 
             val width = activity?.window?.decorView?.width ?: 0
 
@@ -97,17 +114,59 @@ class GhadiPickerFragment : DialogFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(GhadiPickerViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(CalendarDialogViewModel::class.java)
         viewModel.getCalendarData().observe(this, androidx.lifecycle.Observer {
+            adjustRecyclerViewHeight(it)
             adapter.setDataList(it)
         })
-        viewModel.prepareCalendarData(
-            if (::requestedDate.isInitialized) {
-                requestedDate
+
+        val _date = if (::requestedDate.isInitialized) {
+            requestedDate
+        } else {
+            Date(Calendar.getInstance())
+        }
+
+        viewModel.prepareCalendarData(_date, true)
+        // viewModel.getAdDate(_date, true)
+    }
+
+    /**
+     * Dynamically adjusts the height of [RecyclerView] based on the number of data prepared.
+     */
+    private fun adjustRecyclerViewHeight(itemCount: Int) {
+        val currentHeight = mBinding.nepaliDateList.layoutManager?.height ?: 0
+
+        if (lastHeight == 0) {
+            // we do this only once after the view has been setup
+            // i.e:  if the height is 0 else there is no need to execute this block
+            lastHeight = currentHeight
+
+            // should hold reference to minimum height this will not increase or decrease
+            minHeight = lastHeight
+
+            // additional row height which will be added or deducted
+            addedHeight = currentHeight / 6
+
+        }
+
+        if (itemCount < 43) {
+            // if there are 6 ( 7 * 6 = 42 ) rows do not increment more than the minimum height as it will
+            // add an extra space after the last row; will look bad
+            lastHeight = minHeight
+        } else {
+            // check if additional row height is added but must be added only once
+            if ((lastHeight - addedHeight) == minHeight) {
+                // do not add anymore height because we are already at max
             } else {
-                Date(Calendar.getInstance())
-            }, true
-        )
+                // add the additional height
+                lastHeight = minHeight + addedHeight
+            }
+        }
+
+        // finally set the height to recycler view
+        val lp = mBinding.nepaliDateList.layoutParams
+        lp.height = lastHeight
+        mBinding.nepaliDateList.layoutParams = lp
     }
 
     /**
@@ -121,8 +180,8 @@ class GhadiPickerFragment : DialogFragment() {
         // -----------------------
 
         val year = getRootView().findViewById<TextView>(R.id.gpfYear)
-        year.setBackgroundColor(bgFgColor.first)
-        year.setTextColor(bgFgColor.second)
+        // year.setBackgroundColor(bgFgColor.first)
+        year.setTextColor(bgFgColor.first)
         year.text =
             if (::requestedDate.isInitialized) ConversionUtil.toNepali(requestedDate.convertToNepali().year.toString())
             else ConversionUtil.toNepali(currentDateInNepali.year.toString())
@@ -137,7 +196,7 @@ class GhadiPickerFragment : DialogFragment() {
             if (yearNumber == DateUtils.endNepaliYear - 1) {
                 Ui.tintButtonImage(yPrev, android.R.color.holo_red_dark)
             } else {
-                Ui.tintButtonImage(yNext, android.R.color.white)
+                Ui.tintButtonImage(yNext, R.color.collMat)
             }
 
             if (yearNumber <= DateUtils.startNepaliYear) {
@@ -155,7 +214,7 @@ class GhadiPickerFragment : DialogFragment() {
             if (yearNumber == DateUtils.endNepaliYear - 1) {
                 Ui.tintButtonImage(yNext, android.R.color.holo_red_dark)
             } else {
-                Ui.tintButtonImage(yPrev, android.R.color.white)
+                Ui.tintButtonImage(yPrev, R.color.collMat)
             }
 
             if (yearNumber >= DateUtils.endNepaliYear) {
@@ -172,23 +231,19 @@ class GhadiPickerFragment : DialogFragment() {
         // month button listeners
         // -----------------------
 
-        val month = getRootView().findViewById<TextView>(R.id.gpfMonth)
-        val monthExt = getRootView().findViewById<TextView>(R.id.gpfMonthExt)
-        month.setBackgroundColor(bgFgColor.first)
-        monthExt.setBackgroundColor(bgFgColor.first)
-        month.setTextColor(bgFgColor.second)
-        monthExt.setTextColor(bgFgColor.second)
-        month.text =
-            if (::requestedDate.isInitialized) {
-                DateUtils.getMonthName(requestedDate.convertToNepali().month)
-            } else {
-                DateUtils.getMonthName(currentDateInNepali.month)
-            }
-        monthExt.text = if (::requestedDate.isInitialized) {
+        mBinding.gpfMonth.setTextColor(bgFgColor.first)
+        var extMonth = if (::requestedDate.isInitialized) {
             DateUtils.MONTH_NAMES_MAPPED[requestedDate.convertToNepali().month]
         } else {
             DateUtils.MONTH_NAMES_MAPPED[currentDateInNepali.month]
         }
+        mBinding.gpfMonth.text = if (::requestedDate.isInitialized) {
+            DateUtils.getMonthName(requestedDate.convertToNepali().month)
+        } else {
+            DateUtils.getMonthName(currentDateInNepali.month)
+        }
+
+        mBinding.gpfMonth.text = "${mBinding.gpfMonth.text} ( $extMonth )"
 
         val next = getRootView().findViewById<ImageButton>(R.id.gpfNextMonth)
         next.setOnClickListener {
@@ -196,10 +251,11 @@ class GhadiPickerFragment : DialogFragment() {
             val nYear = getDisplayedYear()
             val upcomingMonthName = DateUtils.getNextMonthName(getDisplayedMonth())
             val upcomingMonthNumber = DateUtils.getMonthNumber(upcomingMonthName)
-            month.text = upcomingMonthName
+            mBinding.gpfMonth.text = upcomingMonthName
             changeDate(Date(nYear, upcomingMonthNumber, 1))
 
-            monthExt.text = DateUtils.MONTH_NAMES_MAPPED[upcomingMonthNumber]
+            extMonth = DateUtils.MONTH_NAMES_MAPPED[upcomingMonthNumber]
+            mBinding.gpfMonth.text = "${mBinding.gpfMonth.text} ( $extMonth )"
         }
 
         val prev = getRootView().findViewById<ImageButton>(R.id.gpfPrevMonth)
@@ -208,10 +264,11 @@ class GhadiPickerFragment : DialogFragment() {
             val nYear = getDisplayedYear()
             val upcomingMonthName = DateUtils.getPreviousMonthName(getDisplayedMonth())
             val upcomingMonthNumber = DateUtils.getMonthNumber(upcomingMonthName)
-            month.text = upcomingMonthName
+            mBinding.gpfMonth.text = upcomingMonthName
             changeDate(Date(nYear, upcomingMonthNumber, 1))
 
-            monthExt.text = DateUtils.MONTH_NAMES_MAPPED[upcomingMonthNumber]
+            extMonth = DateUtils.MONTH_NAMES_MAPPED[upcomingMonthNumber]
+            mBinding.gpfMonth.text = "${mBinding.gpfMonth.text} ( $extMonth )"
         }
 
         // -----------------------
@@ -219,8 +276,8 @@ class GhadiPickerFragment : DialogFragment() {
         // -----------------------
 
         val confirm = getRootView().findViewById<Button>(R.id.gpfConfirm)
-        confirm.setBackgroundColor(bgFgColor.first)
-        confirm.setTextColor(bgFgColor.second)
+        // confirm.setBackgroundColor(bgFgColor.first)
+        // confirm.setTextColor(bgFgColor.second)
         confirm.setOnClickListener {
 
             if (!::datePickCompleteListener.isInitialized) throw listenerException
@@ -254,8 +311,8 @@ class GhadiPickerFragment : DialogFragment() {
             dismiss()
         }
         val cancel = getRootView().findViewById<Button>(R.id.gpfCancel)
-        cancel.setBackgroundColor(bgFgColor.first)
-        cancel.setTextColor(bgFgColor.second)
+        // cancel.setBackgroundColor(bgFgColor.first)
+        // cancel.setTextColor(bgFgColor.second)
         cancel.setOnClickListener {
             if (!::datePickCompleteListener.isInitialized) throw listenerException
             dismiss()
@@ -268,6 +325,7 @@ class GhadiPickerFragment : DialogFragment() {
     private fun changeDate(date: Date?) {
         date?.convertToEnglish()?.let {
             viewModel.prepareCalendarData(it)
+            // viewModel.getAdDate(it)
         } ?: showDateUnavailable()
     }
 
@@ -293,7 +351,7 @@ class GhadiPickerFragment : DialogFragment() {
      * Get the root view of this fragment.
      */
     private fun getRootView(): View {
-        return rootView
+        return mBinding.root
     }
 
     /**
@@ -301,8 +359,7 @@ class GhadiPickerFragment : DialogFragment() {
      * @return name of the month currently visible.
      */
     private fun getDisplayedMonth(): String {
-        val month = getRootView().findViewById<TextView>(R.id.gpfMonth)
-        return month.text.toString()
+        return mBinding.gpfMonth.text.toString().split(" ")[0]
     }
 
     /**
